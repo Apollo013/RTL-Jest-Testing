7056a4ecd4e521ba09e511406b04d7f3b628c729
@Apollo013 <paul.j.millar@gmail.com>
2023-09-21 15:09:44 +0100

    Adding test to demostrate getByRole

--- 01-color-button/src/App.test.js
+++ 01-color-button/src/App.test.js
@@ -6,3 +6,9 @@ test("renders learn react link", () => {
     const linkElement = screen.getByText("Learn React");
     expect(linkElement).toBeInTheDocument();
 });
+
+test("renders learn react link (by role)", () => {
+    render(<App />);
+    const linkElement = screen.getByRole("link", { name: "Learn React" });
+    expect(linkElement).toBeInTheDocument();
+});
