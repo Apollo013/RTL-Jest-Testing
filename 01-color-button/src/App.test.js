bbe39c36f3d4cc7ba91e38ac002be3a3343ec247
@Apollo013 <paul.j.millar@gmail.com>
2023-09-25 10:20:56 +0100

    Basic tests added for button & checkbox states

--- 01-color-button/src/App.test.js
+++ 01-color-button/src/App.test.js
@@ -1,14 +1,35 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct inital colour", () => {
+    // Arrange & Act
     render(<App />);
-    const linkElement = screen.getByText("Learn React");
-    expect(linkElement).toBeInTheDocument();
+    const colourButton = screen.getByRole("button", { name: "Change to blue" });
+
+    // Assert
+    expect(colourButton).toHaveStyle({ backgroundColor: "red" });
 });
 
-test("renders learn react link (by role)", () => {
+test("button has correct inital text", () => {});
+
+test("button turns blue when clicked", () => {
+    // Arrange
     render(<App />);
-    const linkElement = screen.getByRole("link", { name: "Learn React" });
-    expect(linkElement).toBeInTheDocument();
+    const colourButton = screen.getByRole("button", { name: "Change to blue" });
+
+    // Act
+    fireEvent.click(colourButton);
+
+    // Assert
+    expect(colourButton).toHaveStyle({ backgroundColor: "blue" });
+    expect(colourButton).toHaveTextContent("Change to red");
+});
+
+test("initial conditions", () => {
+    render(<App />);
+    const colourButton = screen.getByRole("button", { name: "Change to blue" });
+    expect(colourButton).toBeEnabled();
+
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).not.toBeChecked();
 });
